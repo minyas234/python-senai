ab2906cc76f52e2202ae1b753866279dedb40cef
minyas234 <silvadeoliveirayasmin39@gmail.com>
2024-12-05 09:54:12 -0300

    aula02 python

---
+++ Aula 2/aula02.py
@@ -0,0 +1,10 @@
+print('quero minha casa') 
+
+nome_usuario = 'Yasmin' # tipo-> str = string
+idade_usuario = 17 # tipo-> int = integer
+altura_usuario = 1.70 # tipo -> float = ponto flutuante
+maior_idade = False # tipo -> bool = boolean
+print('seja bem-vindo', nome_usuario)
+print (' sua idade é', idade_usuario , 'anos')
+print ('voce tem', altura_usuario ,'metros')
+print ('voce e maior de idade?', maior_idade)
